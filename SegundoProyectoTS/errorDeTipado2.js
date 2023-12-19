5d70beacaea139c25d08d6370b9701dfe2fcc92a
al3xHub <126409718+al3xHub@users.noreply.github.com>
2023-12-19 13:14:53 +0100

    Delete SegundoProyectoTS directory

--- SegundoProyectoTS/errorDeTipado2.js
+++
@@ -1,8 +0,0 @@
-// Función
-function sumaConTipo(cifra01, cifra02) {
-    return cifra01 + cifra02;
-}
-// Uso de la función sumaConTipo
-var primeraCifraConTipo = 1;
-var segundaCifraConTipo = 3;
-var cadenaDeCaracteresConTipo = "abc"; // Error en la transcompilación
--- SegundoProyectoTS/pitufos.js
+++
@@ -1,9 +0,0 @@
-function traductorPitufo(palabra) {
-    return "Pitufos";
-}
-var frase = "Cuando un pitufo se hace un morado es igual que si nosotros no nos hacemos nada";
-var tabPalabras = frase.split(' ');
-for (var i = 0; i < tabPalabras.length; i++) {
-    console.log(traductorPitufo(tabPalabras[i]));
-}
-traductorPitufo(frase);
